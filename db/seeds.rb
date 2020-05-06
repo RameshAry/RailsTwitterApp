a61115f779f72b72be68f3744f668a660ecd64e8
RameshHolly <rameshbaglung90@gmail.com>
2020-05-06 13:19:43 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Create a main sample user.
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -7,22 +7,29 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-# Generate a bunch of additional users.
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
-# Generate microposts for a subset of users.
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentence(word_count: 5)
+  content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Create following relationships.
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
